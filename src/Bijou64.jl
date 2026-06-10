12ea81113c5b08a21fafcf5b18041ccc8b36f7a3
Kyle S. Johnston <KyleSJohnston@users.noreply.github.com>
2026-06-10 19:30:55 -0400

    encode/decode on byte arrays

--- src/Bijou64.jl
+++ src/Bijou64.jl
@@ -20,113 +20,105 @@ function _tier2offset(tier::Integer)::UInt8
     end
 end
 
+const OFFSETS = (
+    0x0000_0000_0000_0000,
+    0x0000_0000_0000_00F8,  # 248
+    0x0000_0000_0000_01F8,
+    0x0000_0000_0001_01F8,
+    0x0000_0000_0101_01F8,
+    0x0000_0001_0101_01F8,
+    0x0000_0101_0101_01F8,
+    0x0001_0101_0101_01F8,
+    0x0101_0101_0101_01F8,
+)
 
-function tier2offset(tier::Integer)::UInt64
-    if tier == 0
+tier2offset(tier::T) where {T <: Unsigned} = OFFSETS[tier + one(T)]
+
+function value2tier(v::T) where {T <: Unsigned}
+    if T(0x00) ≤ v < T(0xF8)
         return 0x00
-    elseif tier == 1
-        return 0xF8  # 248
-    elseif tier == 2
-        return 0x01F8
-    elseif tier == 3
-        return 0x0101F8
-    elseif tier == 4
-        return 0x010101F8
-    elseif tier == 5
-        return 0x01010101F8
-    elseif tier == 6
-        return 0x0101010101F8
-    elseif tier == 7
-        return 0x010101010101F8
-    elseif tier == 8
-        return 0x01010101010101F8
+    elseif T(0xF8) ≤ v < T(0x01F8)
+        return 0x01
+    elseif T(0x01F8) ≤ v < T(0x0101F8)
+        return 0x02
+    elseif T(0x0101F8) ≤ v < T(0x010101F8)
+        return 0x03
+    elseif T(0x010101F8) ≤ v < T(0x01_010101F8)
+        return 0x04
+    elseif T(0x01_010101F8) ≤ v < T(0x0101_010101F8)
+        return 0x05
+    elseif T(0x0101_010101F8) ≤ v < T(0x010101_010101F8)
+        return 0x06
+    elseif T(0x010101_010101F8) ≤ v < T(0x01010101_010101F8)
+        return 0x07
     else
-        throw(ArgumentError("invalid tier $tier"))
-    end
-end
-
-function value2tier(v::Unsigned)::UInt8
-    if 0x00 ≤ v < 0xF8
-        return 0
-    elseif 0xF8 ≤ v < 0x01F8
-        return 1
-    elseif 0x01F8 ≤ v < 0x0101F8
-        return 2
-    elseif 0x0101F8 ≤ v < 0x010101F8
-        return 3
-    elseif 0x010101F8 ≤ v < 0x01_010101F8
-        return 4
-    elseif 0x01_010101F8 ≤ v < 0x0101_010101F8
-        return 5
-    elseif 0x0101_010101F8 ≤ v < 0x010101_010101F8
-        return 6
-    elseif 0x010101_010101F8 ≤ v < 0x01010101_010101F8
-        return 7
-    else
-        return 8
+        return 0x08
     end
 end
 
 
-function decode(io::IO, ::Type{T}) where {T <: Unsigned}
-    tagbyte = try
-        Base.read(io, UInt8)
-    catch e
-        if e isa EOFError
-            throw(BufferTooShort())
+function decode!(results::Vector{T}, bytes::Vector{UInt8}) where {T <: Unsigned}
+    if length(bytes) == 0
+        throw(BufferTooShort())
+    end
+
+    payload_bytes = zeros(UInt8, sizeof(T))
+    i = firstindex(bytes)
+    n = lastindex(bytes)
+    while i ≤ n
+        tagbyte = bytes[i]
+
+        if tagbyte < 0xf8  # 248
+            push!(results, tagbyte)
         else
-            rethrow()
+            tier = tagbyte - 0xf7  # 247
+            padding = sizeof(T) - tier
+            for j in eachindex(payload_bytes)
+                if j ≤ padding
+                    payload_bytes[j] = 0x00
+                else
+                    i = nextind(bytes, i)
+                    try
+                        payload_bytes[j] = bytes[i]
+                    catch e
+                        if e isa BoundsError
+                            throw(BufferTooShort())
+                        else
+                            rethrow()
+                        end
+                    end
+                end
+            end
+            payload_array::Vector{T} = reinterpret(T, payload_bytes)
+            payload = ntoh(payload_array[1])
+            value = tier2offset(tier) + payload
+            if tier == 8 && value < payload
+                throw(OverflowError("overflow detected"))
+            end
+            push!(results, value)
         end
-    end
-
-    if tagbyte < tier2offset(1)
-        return tagbyte
-    else
-        tier = tagbyte - (tier2offset(1) - 1)
-        payload_bytes::Vector{UInt8} = Base.read(io, tier)
-        if length(payload_bytes) != tier
-            # fewer than `tier` bytes were read
-            throw(BufferTooShort())
-        end
-        while length(payload_bytes) < sizeof(T)
-            # big-endian --> add padding to the front
-            pushfirst!(payload_bytes, 0x00)
-        end
-        payload_array::Vector{T} = reinterpret(T, payload_bytes)
-        payload = ntoh(payload_array[1])
-        value = tier2offset(tier) + payload
-        if tier == 8 && value < payload
-            throw(OverflowError("overflow detected"))
-        end
-        return value
-    end
-end
-
-function decode(io::IO, ::Type{Vector{T}}) where {T <: Unsigned}
-    results = T[]
-    while !eof(io)
-        push!(results, decode(io, T))
+        i = nextind(bytes, i)
     end
     return results
 end
 
-
-function encode(io::IO, v::Unsigned)
-    if v < 248
-        Base.write(io, convert(UInt8, v))
-    else
-        tier = value2tier(v)
-        Base.write(io, tier2tag(tier))
-        payload = hton(v - tier2offset(tier))  # big-endian unsigned integer
-        Base.write(io, reinterpret(UInt8, [payload])[end-tier+1 : end])
-    end
+function decode(::Type{T}, bytes::Vector{UInt8}) where {T <: Unsigned}
+    results = T[]
+    decode!(results, bytes)
 end
 
-function encode(io::IO, values::Vector{T}) where {T<:Unsigned}
-    for v in values
-        encode(io, v)
+
+function encode(v::T) where {T <: Unsigned}
+    if v < T(248)
+        return [convert(UInt8, v)]
     end
+    tier = value2tier(v)
+    tag = tier2tag(tier)
+    payload = hton(v - tier2offset(tier))  # big-endian unsigned integer
+    payload_bytes = reinterpret(UInt8, [payload])[end-tier+1 : end]
+    return UInt8[tag, payload_bytes...]
 end
+
 
 
 end # module Bijou64
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,19 +4,8 @@ using Test
 @testset "Tests from Specification" begin
 
     # https://github.com/inkandswitch/bijou/blob/main/bijou64/SPEC.md#worked-example
-    let io = IOBuffer()
-        Bijou64.encode(io, UInt64(67_000))
-        seekstart(io)
-        bytes = take!(io)
-        @test bytes == [0xFA, 0x00, 0x03, 0xC0]
-    end
-
-    let io = IOBuffer()
-        Bijou64.encode(io, UInt64(67_000))
-        seekstart(io)
-        @test Bijou64.decode(io, UInt64) == 67_000
-        @test position(io) == 4
-    end
+    @test Bijou64.encode(UInt64(67_000)) == [0xFA, 0x00, 0x03, 0xC0]
+    @test Bijou64.decode(UInt64, [0xFA, 0x00, 0x03, 0xC0]) == [67_000]
 
     # https://github.com/inkandswitch/bijou/blob/main/bijou64/SPEC.md#test-vectors
 
@@ -63,31 +52,14 @@ using Test
     ]
 
     for (v, b) in zip(values, bytes)
-        let io = IOBuffer()
-            Bijou64.encode(io, v)
-            seekstart(io)
-            result = take!(io)
-            @test result == b
-        end
-        let io = IOBuffer()
-            Bijou64.encode(io, v)
-            seekstart(io)
-            @test Bijou64.decode(io, UInt64) == v
-        end
+        @test Bijou64.encode(v) == b
+        @test Bijou64.decode(UInt64, b) == [v]
     end
 
     # https://github.com/inkandswitch/bijou/blob/main/bijou64/SPEC.md#error-conditions
 
-    let io = IOBuffer()
-        @test_throws BufferTooShort Bijou64.decode(io, UInt64)
-    end
-
-    let io = IOBuffer(UInt8[0xF9, 0x00])
-        @test_throws BufferTooShort Bijou64.decode(io, UInt64)
-    end
-
-    let io = IOBuffer(UInt8[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-        @test_throws OverflowError Bijou64.decode(io, UInt64)
-    end
+    @test_throws BufferTooShort Bijou64.decode(UInt64, UInt8[])
+    @test_throws BufferTooShort Bijou64.decode(UInt64, UInt8[0xF9, 0x00])
+    @test_throws OverflowError Bijou64.decode(UInt64, UInt8[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
 
 end
